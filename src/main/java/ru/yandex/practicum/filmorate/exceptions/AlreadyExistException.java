5cc110ae60c978715d16120c9ef72198334d2635
Luckaaa <perepelitsalesha@yandex.ru>
2024-03-05 20:04:13 +0300

    refactor: add whitespace gefore '{'

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/AlreadyExistException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/AlreadyExistException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-public class AlreadyExistException extends Throwable{
+public class AlreadyExistException extends Throwable {
     public AlreadyExistException(String message) {
         super(message);
     }
